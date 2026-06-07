4780ad5cfe6c2de3d82689a5c1390d31d77da3ab
Jayaprakash S <prakash4830jp@gmail.com>
2026-06-07 14:00:01 +0530

    added 30 sec countdown

--- src/components/CountdownScreen.jsx
+++ src/components/CountdownScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FiHeart } from 'react-icons/fi';
 
 // const BIRTHDAY_TIME = new Date('2026-06-07T18:30:00.000Z');
-const BIRTHDAY_TIME = new Date(Date.now() + 20 * 1000);
+const BIRTHDAY_TIME = new Date(Date.now() + 30 * 1000);
 
 const DEV_BYPASS = false;
 const POST_COUNTDOWN_DELAY = 6000;
